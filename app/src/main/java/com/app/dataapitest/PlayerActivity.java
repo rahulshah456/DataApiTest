ca9bd3e2173e92cea12d56cbd16538deecc07e2a
rahulshah456 <rahulkumarshah5000@gmail.com>
2019-05-30 11:07:40 +0530

    YT player added

--- app/src/main/java/com/app/dataapitest/PlayerActivity.java
+++ app/src/main/java/com/app/dataapitest/PlayerActivity.java
@@ -5,7 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class PlayerActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import Utils.Constants;
+
+public class PlayerActivity extends YouTubeBaseActivity {
+
+    YouTubePlayer.OnInitializedListener onInitializedListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +22,27 @@ public class PlayerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_player);
 
         Intent intent = getIntent();
-        String id = intent.getStringExtra("Id");
+        final String id = intent.getStringExtra("Id");
         Log.d("Id:",id);
+
+        final YouTubePlayerView playerView = (YouTubePlayerView) findViewById(R.id.playerId);
+
+
+
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+                youTubePlayer.loadVideo(id);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        playerView.initialize(Constants.YOUTUBE_API_KEY,onInitializedListener);
+
     }
 }
